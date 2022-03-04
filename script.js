340240870a12a2670dfbdaa7fc2f22bf1ffb14c4
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-03-04 10:47:07 +0000

    css styling, equals button and equation buttons now working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let display = document.querySelector("#display");
+const display = document.querySelector("#display");
 const numberButtons = document.querySelectorAll(".number-btn");
-const functionButtons = document.querySelectorAll("function-btn")
+const equationButtons = document.querySelectorAll(".equation-btn")
 const allButtons = document.querySelectorAll("button")
 const clearButton = document.querySelector("#clear")
 const addButton = document.getElementById("add")
@@ -9,33 +9,67 @@ const percentButton = document.getElementById("percent")
 const divideButton = document.getElementById("divide")
 const equalsButton = document.getElementById("equals")
 const mathFunction = document.querySelectorAll(".math-function")
+const inputLengthCheck = () => {
+    if (display.innerHTML.length<15) {
+        inputLengthWithinLimit = true;
+    } else {
+        inputLengthWithinLimit = false;
+    }
+}
 
 let currentCharacter = "";
 
-// let sum = []
-// const selectedButton = display.innerHTML
 
-// INPUT 1
-// const input1 = 
-// INPUT 2
 
-//This function needs to return a number to the display when the number buttons are clicked
+// SHOW UP SELCTED NUMBER BUTTON ON DISPLAY AND CHARACTER LIMIT TO 15
 numberButtons.forEach(button => {
     button.addEventListener(("click"), (event) => {
-        currentCharacter = event.target.innerHTML;
-        display.innerHTML += event.target.innerHTML;
+        inputLengthCheck();
+        if (inputLengthWithinLimit === true) {
+            currentCharacter = event.target.innerHTML;
+            display.innerHTML += currentCharacter;
+        }
     })
+}); 
+
+
+
+// EQUATION BUTTONS
+// equationButtons.forEach(button => {
+//     button.addEventListener(("click"), (event) => {
+//         if (display.innerHTML.includes("+"||"-"||"*"||"/")=== false) {
+//             currentCharacter = event.target.innerHTML;
+//             display.innerHTML += event.target.innerHTML;
+//         }
+//         else {
+//             display.innerHTML = "";
+            
+//        }
+ 
+//  }) })  
+
+// const equationButtonsNew = ["+", "-", "*", "/"];
+//  equationButtons.forEach(button => {
+//     button.addEventListener(("click"), (event) => {
+//         if (display.innerHTML.includes(equationButtonsNew)) {   
+//             currentCharacter = event.target.innerHTML;  
+//         }
+//         else  {
+//         currentCharacter = event.target.innerHTML;
+//         display.innerHTML += event.target.innerHTML;}
+//     })
+// })
+
+equationButtons.forEach(button => {
+   button.addEventListener(("click"), (event) => {
+       if (display.innerHTML.includes("+")) {   
+           currentCharacter = event.target.innerHTML;  
+       }
+       else  {
+       currentCharacter = event.target.innerHTML;
+       display.innerHTML += event.target.innerHTML;}
+   })
 })
-
-//
-mathFunction.forEach(button => {
-    button.addEventListener(("click"), (event) => {
-        currentCharacter = event.target.innerHTML;
-        display.innerHTML += event.target.innerHTML;
-    })
-})
-
-
 // CLEAR BUTTON
 clearButton.addEventListener(("click"), () => {
     if (currentCharacter == "C") {
@@ -48,59 +82,28 @@ clearButton.addEventListener(("click"), () => {
 })
 
 
+// EQUALS BUTTON 
+equalsButton.addEventListener("click", () => {
+    if (display.innerHTML.includes("x")== true) {
+        calculation = display.innerHTML.split("x");
+        calculationResult = Number(calculation[0]) * Number(calculation[1]);
+    }
+    else if (display.innerHTML.includes("÷")== true) {
+        calculation = display.innerHTML.split("÷");
+        calculationResult = Number(calculation[0]) / Number(calculation[1]);
+    }
+    else if (display.innerHTML.includes("-")== true) {
+        calculation = display.innerHTML.split("-");
+        calculationResult = Number(calculation[0]) - Number(calculation[1]);
+    }
+    else if (display.innerHTML.includes("+")== true) {
+        calculation = display.innerHTML.split("+");
+        calculationResult = Number(calculation[0]) + Number(calculation[1]);
+    }
+    display.innerHTML = calculationResult;
+})
 
-
-//ADDITON FUNCTION
-// For addition I need the display values to be added together when the calculate
- //button is selceted
-// addButton.addEventListener(("click"), () => {
-//   const totalSum = [`${display.innerHTML} +`];
-//     display.innerHTML = totalSum;
-// })
-
-// //MINUS FUNCTION
-// minusButton.addEventListener(("click"), () => {
-//     const totalSum = [`${display.innerHTML} -`];
-//       display.innerHTML = totalSum;
-//   })
-
-
-// // EQUALS FUNCTION
-// equalsButton.addEventListener(("click"), () => {
-//     currentCharacter += `${display.innerHTML}`;
-//     const finalSum = 
-// })
-
-// export const totalScoresArr = (scoresArr) => {
-//   const totalScore = scoresArr.reduce((total, current) => (total += current), 0);
-//   return totalScore;
-
-// a function that just displays the function symbols
-
-// then a function that runs the symbols with the numbers when = button clicked
-// functionAdd = display.value + display.value 
-
-// addBtn.addEventListener("click", functionAdd);
-
-// Totals numbers
-//  * A function that uses the REDUCE array iterator to total an array of scores.
-//  * The scores will be numbers.
-//  *
-//  * @param {number[]} numberArr [7, 7, 6, 2, 3, 2, 3]
-//  * @return {number} 30
-//  */
-// export const totalScoresArr = (scoresArr) => {
-//   const totalScore = scoresArr.reduce((total, current) => (total += current), 0);
-//   return totalScore;
-
-
-
-///numberButtons.addEventListener("click", () => display.innerHTML = 0)
-
-
-// //num1Btn.addEventListener("click", () => {num1btn.style.backgroundcolor = "red"})
-
-// % code as seperate one ie times by 100
-// operators 
-
-//if you press cancel what needs to happed? if you press cencel twice. 
+ // PERCENT BUTTON 
+percentButton.addEventListener("click", () => {
+    display.innerHTML  = display.innerHTML/100;
+})
